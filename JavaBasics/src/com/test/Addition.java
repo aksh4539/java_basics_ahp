382376206991ea5bf0545c634755735ea6cab71c
aksh4539 <akash4539@gmail.com>
2016-07-17 11:26:55 -0500

    Added new version of Addition file
    Added new version of Addition file

---
+++ JavaBasics/src/com/test/Addition.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		addTwoNumbers();
+	}
+	
+	public static void addTwoNumbers() {
+		int no1 = 10;
+		int no2 = 20;
+		int sum = no1+no2;
+		System.out.println("sum of 2 numbers:" + sum);
+	}
+
+}
